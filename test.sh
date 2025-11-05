d85f51d1915d9fd26ba4c1c62929f2f54dfc630d
vatsal1306 <vatsal1399@gmail.com>
2025-11-05 20:17:42 +0530

    add inference tests for last and best model outputs

--- test.sh
+++ test.sh
@@ -58,3 +58,37 @@ python src/viz_predictions.py \
 --mistake_type safe_as_not_safe
 
 
+
+# Test on inference data
+python src/inference.py \
+-i dataset/infer_data/ \
+-o runs/${RUN_NAME}/infer_last/infer_data/ \
+-c runs/${RUN_NAME}/checkpoints/${CHKPT_NAME} \
+-m efficientnet_v2_s \
+-b 64 \
+-w 8
+
+# for best model
+python src/inference.py \
+-i dataset/infer_data/ \
+-o runs/${RUN_NAME}/infer_best/infer_data/ \
+-c runs/${RUN_NAME}/checkpoints/${BEST_CHKPT_NAME} \
+-m efficientnet_v2_s \
+-b 64 \
+-w 8
+
+
+
+## save grids
+#python save_grids.py \
+#-i runs/${RUN_NAME}/infer/incorrect_preds/ \
+#-o runs/${RUN_NAME}/infer/grids/incorrect_preds \
+#--img_size 752 512 \
+#--num_workers 32
+#
+#
+#python save_grids.py \
+#-i runs/${RUN_NAME}/infer/infer_data/ \
+#-o runs/${RUN_NAME}/infer/grids/infer_data \
+#--img_size 732 512 \
+#--num_workers 8
